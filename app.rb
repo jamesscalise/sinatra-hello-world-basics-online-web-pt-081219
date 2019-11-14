f213a892dc8f715bbc1e56199e069a31611c8471
James Scalise <jamescscalise@gmail.com>
2019-11-14 20:04:47 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,8 @@
 class App < Sinatra::Base
 
   # Define your GET '/' route below and respond with "Hello, World!"
-  https://www.youtube.com/watch?v=Z-JA--NZYqQ
+  get '/' do 
+    "Hello, World!"
+  end
 
 end
